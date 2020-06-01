4f0ddeb83ee1e0911aa36f9e1dcb834c421a793b
EC2 Default User <ec2-user@ip-172-31-32-56.us-east-2.compute.internal>
2020-06-01 05:03:24 +0000

    Se agregan funciones de backend al game_controller y esqueletos para jugador_controller

---
+++ backend/controllers/juegador_controller.js
@@ -0,0 +1,10 @@
+const Jugador = require("../models jugador_model")
+const mongoose = require("moongoose")
+
+exports.getCartas = (req, res) =>{
+    
+}
+
+exports.getPedirCarta = (req, res) =>{
+    
+}
--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose=require('mongoose');
+const Carta = require("./models/carta.js")
+const carta = Carta.carta
 const app = express()
-
-
-    
 
 mongoose.connect('mongodb://localhost/21blackjack',{useNewUrlParser: true,useUnifiedTopology: true})
 .then(result=>{
